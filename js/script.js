70f516481925626f9b74a2dda735f55483f8ca13
Emmanuel / <emmanuelsuareztou@gmail.com>
2023-09-22 23:00:38 -0300

    detailsss

--- js/script.js
+++ js/script.js
@@ -13,21 +13,45 @@ let ans = "";
 let historyArray = [];
 
 function operationFunction(a, b, operator) {
+  //Se compara el operador ingresado
+  //para realizar la operacion correspondiente y se valida
+  //que no se divida por 0.
+
   if (operator === "+") {
-    result.innerHTML = a + b;
+    let res = a + b;
+    if (res.toString.length < 20) {
+      result.innerHTML = res;
+    } else {
+      result.innerHTML = res.toPrecision(20);
+    }
     ans = result.innerHTML;
   } else if (operator === "-") {
-    result.innerHTML = a - b;
+    let res = a - b;
+    if (res.toString.length < 20) {
+      result.innerHTML = res;
+    } else {
+      result.innerHTML = res.toPrecision(20);
+    }
     ans = result.innerHTML;
   } else if (operator === "*") {
-    result.innerHTML = a * b;
+    let res = a * b;
+    if (res.toString.length < 20) {
+      result.innerHTML = res;
+    } else {
+      result.innerHTML = res.toPrecision(20);
+    }
     ans = result.innerHTML;
   } else if (operator === "/") {
     if (b === 0) {
       result.innerHTML = "Can't divide by 0.";
       ans = 0;
     } else {
-      result.innerHTML = a / b;
+      let res = a / b;
+      if (res.toString.length < 20) {
+        result.innerHTML = res;
+      } else {
+        result.innerHTML = res.toPrecision(20);
+      }
       ans = result.innerHTML;
     }
   }
@@ -60,36 +84,57 @@ function addToHistory(obj) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  //Agarramos todos los botones de numeros y operadores.
+
   const numKeys = document.querySelectorAll(".numKey");
   const operatorKeys = document.querySelectorAll(".operatorKey");
 
+  //Event listeners para los botones de operaiones.
+  //Al presionarlos, se agrega el simbolo de la operacion al input (parte de arriba del display),
+  //y se guardan los valores del numero ingresado antes del operador y que operador se selecciono.
   operatorKeys.forEach((key) => {
     key.addEventListener("click", () => {
       numInput.innerHTML += key.innerHTML;
       operation = key.innerHTML;
-      leftHand = numInput.innerHTML.slice(0, -1);
 
-      console.log(leftHand.length);
+      //Al presionar las teclas se va formando un string adentro del input.
+      //Para poder agarrar el numero ingresado antes del operador,
+      //se usa slice para agarrar el string desde el principio hasta
+      //el ultimo caracter antes del operador.
+      leftHand = numInput.innerHTML.slice(0, -1);
     });
   });
 
+  //Event listeners para los botones de numeros.
+  //Al presionarlos, se agrega el numero al input (parte de arriba del display).
   numKeys.forEach((key) => {
     key.addEventListener("click", () => {
       numInput.innerHTML += key.innerHTML;
     });
   });
 
+  //Event listeners para los botones de operaciones especiales.
   equal.addEventListener("click", () => {
-    let a = parseFloat(leftHand);
+    let a = parseFloat(leftHand); //el numero ingresado antes del operador.
+
+    //Para conseguir el numero ingresado despues del operador pensé de la siguiente forma:
+    //El primer dígito del segundo numero ingresado despues del operador
+    //siempre va a estar despues del largo del primero numero, osea,
+    //cuantos dígitos tiene el primer numero ingresado (lefthand), + 1 porque
+    //no precisamos el operador, y como slice no incluye el caracter en la posicion de inicio,
+    //podemos decirle que comience desde la posicion operador.
     let b = parseFloat(numInput.innerHTML.slice(leftHand.length + 1));
 
+    //Se llama a la funcion que realiza la operacion con los dos numeros ingresados y el operador.
     operationFunction(a, b, operation);
 
+    //Se crea un objeto con el input y el output de la operacion realizada para el historial.
     let newEntry = {
       input: numInput.innerHTML,
       output: result.innerHTML,
     };
 
+    //Se llama a la funcion que agrega la entrada al historial con el último resultado.
     addToHistory(newEntry);
   });
 
@@ -104,7 +149,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   ansButton.addEventListener("click", () => {
-    numInput.innerHTML = ans;
+    numInput.innerHTML += ans;
   });
 
   historyClear.addEventListener("click", () => {
