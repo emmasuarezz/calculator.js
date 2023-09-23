bd1c08895986e0da6046ac4a42d499f3884dfb4c
Emmanuel / <emmanuelsuareztou@gmail.com>
2023-09-22 23:28:09 -0300

    almost

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ let operation = "";
 let leftHand = "";
 let ans = "";
 let historyArray = [];
+let equalKeyPressed = false;
 
 function operationFunction(a, b, operator) {
   //Se compara el operador ingresado
@@ -60,9 +61,14 @@ function operationFunction(a, b, operator) {
 function addToHistory(obj) {
   let htmlContentToAppend = "";
 
+  //Cambiamos el mensaje de error para que se vea mejor en el historial.
+
   if (obj.output === "Can't divide by 0.") {
     obj.output = "Error.";
   }
+
+  //Se agrega la entrada al historial y se valida que no se agreguen mas de 5 entradas.
+  //Si se agregan mas de 5, se elimina la ultima entrada y se agrega la nueva al principio.
 
   if (historyArray.length <= 4) {
     historyArray.unshift(obj);
@@ -70,6 +76,8 @@ function addToHistory(obj) {
     historyArray.pop();
     historyArray.unshift(obj);
   }
+
+  //Se crea el html para agregar al historial ciclando por el array de entradas.
 
   historyArray.forEach((element) => {
     htmlContentToAppend += `
@@ -79,6 +87,8 @@ function addToHistory(obj) {
           </div>
           `;
   });
+
+  //Se agrega el html al historial.
 
   document.getElementById("historyContainer").innerHTML = htmlContentToAppend;
 }
@@ -94,8 +104,19 @@ document.addEventListener("DOMContentLoaded", () => {
   //y se guardan los valores del numero ingresado antes del operador y que operador se selecciono.
   operatorKeys.forEach((key) => {
     key.addEventListener("click", () => {
-      numInput.innerHTML += key.innerHTML;
-      operation = key.innerHTML;
+      //Se valida que no se presione un operador si no hay un numero ingresado.
+
+      if (numInput.innerHTML === "") {
+        numInput.innerHTML = "";
+      } else {
+        if (equalKeyPressed) {
+          equalKeyPressed = false;
+          operation = key.innerHTML;
+          numInput.innerHTML = ans;
+        }
+        operation = key.innerHTML;
+        numInput.innerHTML += operation;
+      }
 
       //Al presionar las teclas se va formando un string adentro del input.
       //Para poder agarrar el numero ingresado antes del operador,
@@ -107,14 +128,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //Event listeners para los botones de numeros.
   //Al presionarlos, se agrega el numero al input (parte de arriba del display).
+
+  //S
+
   numKeys.forEach((key) => {
     key.addEventListener("click", () => {
-      numInput.innerHTML += key.innerHTML;
+      if (equalKeyPressed) {
+        numInput.innerHTML = key.innerHTML;
+        equalKeyPressed = false;
+      } else {
+        numInput.innerHTML += key.innerHTML;
+      }
     });
   });
 
   //Event listeners para los botones de operaciones especiales.
   equal.addEventListener("click", () => {
+    //Se valida que se presionó el boton de igual para cambiar el comportamiento
+    //de la calculadora en caso de que se presione un numero despues.
+    equalKeyPressed = true;
+
     let a = parseFloat(leftHand); //el numero ingresado antes del operador.
 
     //Para conseguir el numero ingresado despues del operador pensé de la siguiente forma:
@@ -149,7 +182,11 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   ansButton.addEventListener("click", () => {
-    numInput.innerHTML += ans;
+    if (equalKeyPressed) {
+      numInput.innerHTML = ans;
+    } else {
+      numInput.innerHTML += ans;
+    }
   });
 
   historyClear.addEventListener("click", () => {
